2d31c547e876a8495d1fa298b6468e421ae74858
larryjiang <larry567.jiang@gamil.com>
2020-11-11 19:07:57 +0800

    init

---
+++ vscode_git_practice/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for vscode_git_practice project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vscode_git_practice.settings.dev")
+
+application = get_wsgi_application()
